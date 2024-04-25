ae505f40dba1c2a11b7d95c2432cfcb2facc1c8e
siddharth <144979580+Sid141004@users.noreply.github.com>
2024-04-25 18:12:33 +0530

    Update recursion_2.c

--- PSUC workshop/recursion_2.c
+++ PSUC workshop/recursion_2.c
@@ -1,45 +1,17 @@
-// C Program to print the Fibbonaci series
 #include <stdio.h>
-int prev1 = 1;
-int prev2 = 0;
 
-// recursive function
-void fib(int n)
+unsigned int factorial(unsigned int n)
 {
-	if (n < 3) {
-		return;
+	if (n == 1) {
+	return 1;
 	}
-	int fn = prev1 + prev2;
-	prev2 = prev1;
-	prev1 = fn;
-	printf("%d ", fn);
-	return fib(n - 1);
+
+	return n * factorial(n - 1);
 }
 
-void printFib(int n)
-{
-	//n=0
-	if (n < 1) {
-		printf("Invalid number of terms\n");
-	}
-	//n=1
-	else if (n == 1) {
-		printf("%d ", 0);
-	}
-	//n=2
-	else if (n == 2) {
-		printf("%d %d", 0, 1);
-	}
-	//n>2
-	else {
-		printf("%d %d ", 0, 1);
-		fib(n);
-	}
-	return;
-}
 int main()
 {
-	int n = 9;
-	printFib(n);
+	int num = 5;
+	printf("Factorial of %d is %d", num, factorial(num));
 	return 0;
 }
